c1437a700dae181e1f2dc024fffab0a168ce72c6
FatBoyLong <eugggggene@gmail.com>
2024-01-21 14:37:07 +0300

    styled hystor rows

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -14,12 +14,14 @@ function HistoryPage() {
       <div className="flex flex-col gap-2 overflow-scroll">
         {history.map((el) => (
           <div
-            className="text-l flex items-center justify-between gap-2 rounded-md  bg-slate-200/60 px-2 py-1 text-center font-medium dark:bg-slate-800"
+            className="text-l flex items-center justify-between gap-4 rounded-md  bg-slate-200/60 px-2 py-1 text-center font-medium dark:bg-slate-800"
             key={el.identificator}
           >
             <img src={el.gifUrl} alt={el.name} className="h-10 rounded-md" />
-            <p>{el.name}</p>
-            <div>
+            <p className="basis-1/4 text-sm uppercase">
+              {el.name.length > 15 ? `${el.name.slice(0, 15)}...` : el.name}
+            </p>
+            <div className="shrink-0 grow-0">
               <div className="flex items-center gap-2">
                 <span>Sets</span>
                 <span>{el.sets}</span>
@@ -29,7 +31,7 @@ function HistoryPage() {
                 <span>{el.reps}</span>
               </div>
             </div>
-            <p>{`${el.date.day}.${
+            <p className="shrink-0 grow-0">{`${el.date.day}.${
               el.date.month <= 9 ? "0" + el.date.month : el.date.month
             }.${el.date.year}`}</p>
             <FaTrashAlt
@@ -41,7 +43,9 @@ function HistoryPage() {
           </div>
         ))}
       </div>
-      <DeleteHistoryButton>Clear all history</DeleteHistoryButton>
+      {history.length > 0 && (
+        <DeleteHistoryButton>Clear all history</DeleteHistoryButton>
+      )}
     </div>
   );
 }
